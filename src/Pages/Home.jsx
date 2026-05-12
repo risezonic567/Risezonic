c6b9620ceb429f198a86fc5e6878cb2d4dfedbbc
Sunny9467 <kumarsunny919200@gmail.com>
2026-05-12 12:02:52 +0530

    footer chnages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,6 +14,7 @@ import HeroSectionHome from "../components/Herosectionhome";
 import FAQ from "../components/FQA";
 import NavForOther from "../components/NavForOther";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   useEffect(() => {
@@ -136,9 +137,9 @@ export default function Home() {
                   <button className="group p-[4px] rounded-[12px] bg-gradient-to-b from-gray-700 to-gray-600 shadow-[0_2px_4px_rgba(0,0,0,0.7)] hover:shadow-[0_4px_8px_rgba(0,0,0,0.6)] active:shadow-[0_0px_1px_rgba(0,0,0,0.8)] active:scale-[0.995] transition-all duration-200">
                     <div className="bg-gradient-to-b from-gray-600 to-gray-700 rounded-[8px] px-3 py-2">
                       <div className="flex gap-2 items-center">
-                        <span className="font-semibold text-white">
+                        <Link to="/about-us" className="font-semibold text-white">
                           More Details
-                        </span>
+                        </Link>
                       </div>
                     </div>
                   </button>
